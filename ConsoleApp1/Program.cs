bac0006f3a91641642d69b206c5aa59a60de8ac0
shruti270 <patilshruti270@gmail.com>
2025-09-26 17:42:22 +0530

    Added CRM Dev to the string

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            "Hello World, Welcome to the team"
+            "Hello World, Welcome to the team CRM DEV"
         }
     }
 }
